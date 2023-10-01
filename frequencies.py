e3c15e9043094e6f18cfc32b9add8c612ecdcf25
k22007695 <k22007695@kcl.ac.uk>
2023-10-01 20:59:56 +0100

    Working version

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+from collections import Counter
+def stringList (items):
+    return [str(i) for i in items]
+
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items = stringList(items)
+    frequencies=dict(Counter(items))
     return frequencies
